0ed5d18211e3f64c957a84440130a05da9bc2c4d
Jacob Wasilkowski <jwasilkowski@esri.com>
2016-10-19 13:52:10 -0500

    added intial repo directories and files

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "browser": true
+  },
+  "globals": {
+    "define": false,
+    "require": false
+  },
+  "rules": {
+    "no-debugger": "error",
+    "no-bitwise": "error",
+    "curly": "error",
+    "eqeqeq": "error",
+    "guard-for-in": "error",
+    "block-scoped-var": "off",
+    "no-use-before-define": "warn",
+    "max-params": "off",
+    "no-caller": "error",
+    "no-new": "error",
+    "no-shadow": "error",
+    "strict": "off",
+    "no-undef": "error",
+    "no-unused-vars": "error",
+    "no-eval": "off",
+    "indent": ["error", 2, {
+      "SwitchCase": 1
+    }]
+  }
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Canvas-Flowline-Layer
